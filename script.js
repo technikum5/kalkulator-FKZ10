3f1c3dd866b484f5c3a6f05500b3e532b45d4723
FKZ10 <filip.kazmierczak@zsmeie.edu.torun.pl>
2024-11-29 17:25:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-function calculate(operation) {
-//Dpkończ kod - pobierz liczby i sprawdź czy są poprawne
-        alert('Proszę wprowadzić prawidłowe liczby.');
-        return;
-    }
+function calculate(operation) 
+    {
+        let x = parseFloat(document.getElementById("number1").value);
+        let y = parseFloat(document.getElementById("number2").value);
 
-    switch (operation) {
-       //Dokończ funkcję działań: dodawania, odejmowania, dzielenia, mnożenia
-       
-             //uzupełnij   alert('Nie można dzielić przez zero.');
-            alert('Nieznana operacja.');
+        if (isNaN(x) || isNaN(y))
+        {
+            alert('Proszę wprowadzić prawidłowe liczby.');
             return;
+        }
+       
+    
+
+    let wynik;
+    switch (operation) 
+    {
+        case "dodawanie":
+            wynik = x + y
+            break;
+        case "odejmowanie":
+            wynik = x-y
+            break;
+        case "mnożenie":
+            wynik = x*y
+            break;
+        case "dziel+nie":
+            if(y === 0)
+            {
+                alert('Nie można dzielić przez zero.');
+                return;
+            }
+            wynik = x / y
+            break;
+   
     }
 
-    document.getElementById('result').textContent = result;
+    document.getElementById('wynik').textContent = wynik;
 }
